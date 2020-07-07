3a80ca636fc45ffebff8034bf0483e2c102fc433
vladoskav <vladoskavkniaz@yandex.ru>
2020-07-07 15:55:25 +0300

    feature: add cloud icons

--- src/js/index.js
+++ src/js/index.js
@@ -33,18 +33,24 @@ const burgerMenu = document.querySelector('.burger-menu');
 const main = document.querySelector('.main');
 const modal = document.querySelector('.modal');
 const popupClose = document.querySelector('.popup__close');
+const lkLink = document.querySelector('.nav-bar__link_lk');
 
-
+const openModal = () => {
+    modal.classList.add('modal_is-opened');
+};
 
 popupClose.addEventListener('click', () => {
 
    modal.classList.remove('modal_is-opened');
 });
 
-lkButton.addEventListener('click', () => {
-     modal.classList.add('modal_is-opened');
+lkLink.addEventListener('click', () => {
+    openModal();
+    burgerMenu.classList.remove('burger-menu_opened');
 
 });
+
+lkButton.addEventListener('click', openModal);
 
 
 burgerButton.addEventListener('click', () => {
