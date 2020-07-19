11a897dc3829f7b4360e461279868a5d39631f3d
vladoskav <vladoskavkniaz@yandex.ru>
2020-07-19 13:26:31 +0300

    feature: add modal windows all pages

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,42 +1,87 @@
 import "normalize.css";
 import "src/scss/questions.scss";
+import Popup from "./popup";
 
+// Селекторы
 const panel = document.querySelectorAll('.accordion__panel');
+
 const lkButton = document.querySelector('.header__lk-button');
 const burgerButton = document.querySelector('.burger-menu__button');
 const burgerMenu = document.querySelector('.burger-menu');
 const main = document.querySelector('.main');
-const modal = document.querySelector('.modal');
-const popupClose = document.querySelector('.popup__close');
+const modalLk = document.querySelector('.modal_lk');
+const popupCloseLk = document.querySelector('.popup__close_lk');
 const lkLink = document.querySelector('.nav-bar__link_lk');
+const startWorkButton = document.querySelector('.header__start-button');
+const modalReg = document.querySelector('.modal_reg');
+const popupCloseReg = document.querySelector('.popup__close_reg');
+const confirmButton = document.querySelector('.popup__button_reg');
+const thanksPopup = document.querySelector('.modal_thanks');
+const popupLink = document.querySelector('.popup__link');
+const modalRec = document.querySelector('.modal_recovery');
+const recButton = document.querySelector('.popup__button_recovery');
+const modalReset = document.querySelector('.modal_reset');
 
-const openModal = () => {
-    modal.classList.add('modal_is-opened');
-};
+// Открытие личного кабинета
+const popupLk = new Popup(modalLk, popupCloseLk);
 
-popupClose.addEventListener('click', () => {
 
-    modal.classList.remove('modal_is-opened');
+lkButton.addEventListener('click', () => {
+    popupLk.open();
 });
 
 lkLink.addEventListener('click', () => {
-    openModal();
+    popupLk.open();
     burgerMenu.classList.remove('burger-menu_opened');
 
 });
 
-lkButton.addEventListener('click', openModal);
+// Открытие регистрации
+const popupReg = new Popup(modalReg, popupCloseReg);
 
+startWorkButton.addEventListener('click', () => {
+    popupReg.open();
+
+});
+
+// Открытие мобильного меню
 
 burgerButton.addEventListener('click', () => {
     burgerMenu.classList.toggle('burger-menu_opened');
 
 });
 main.addEventListener('click', () => {
-    burgerMenu.classList.remove('burger-menu_opened');
+    burgerMenu.classList.remove('burger-menu_opened')
+});
+
+// Открытие формы спасибо
+
+const popupThanks = new Popup(thanksPopup, popupCloseReg);
+
+confirmButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    popupThanks.open();
+    popupReg.close();
+});
+
+// Открытие восстановления пароля
+
+const popupRec = new Popup(modalRec, popupCloseReg);
+
+popupLink.addEventListener('click', () => {
+    popupLk.close();
+    popupRec.open();
 
 });
 
+// Пароль восстановлен
+const popupReset = new Popup(modalReset, popupCloseReg);
+
+recButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    popupReset.open();
+    popupRec.close();
+});
 
 for (let i = 0; i < panel.length; i++) {
     panel[i].addEventListener('click', () => {
--- src/js/rates.js
+++ src/js/rates.js
@@ -1,13 +1,99 @@
 import "normalize.css";
 import "src/scss/rates.scss";
+import Popup from "./popup";
+
 
 const lkButton = document.querySelector('.header__lk-button');
 const burgerButton = document.querySelector('.burger-menu__button');
 const burgerMenu = document.querySelector('.burger-menu');
 const main = document.querySelector('.main');
-const modal = document.querySelector('.modal');
-const popupClose = document.querySelector('.popup__close');
+const modalLk = document.querySelector('.modal_lk');
+const popupCloseLk = document.querySelector('.popup__close_lk');
 const lkLink = document.querySelector('.nav-bar__link_lk');
+const startWorkButton = document.querySelector('.header__start-button');
+const modalReg = document.querySelector('.modal_reg');
+const popupCloseReg = document.querySelector('.popup__close_reg');
+const confirmButton = document.querySelector('.popup__button_reg');
+const thanksPopup = document.querySelector('.modal_thanks');
+const popupLink = document.querySelector('.popup__link');
+const modalRec = document.querySelector('.modal_recovery');
+const recButton = document.querySelector('.popup__button_recovery');
+const modalReset = document.querySelector('.modal_reset');
+
+// Тарифы
+const toPlug = document.querySelectorAll('.rates__to-plug');
+const modalPlug = document.querySelector('.modal_plug');
+const closePlug = document.querySelector('.popup__close_plug');
+// Открытие личного кабинета
+const popupLk = new Popup(modalLk, popupCloseLk);
+
+
+lkButton.addEventListener('click', () => {
+    popupLk.open();
+});
+
+lkLink.addEventListener('click', () => {
+    popupLk.open();
+    burgerMenu.classList.remove('burger-menu_opened');
+
+});
+
+// Открытие регистрации
+const popupReg = new Popup(modalReg, popupCloseReg);
+
+startWorkButton.addEventListener('click', () => {
+    popupReg.open();
+
+});
+
+// Открытие мобильного меню
+
+burgerButton.addEventListener('click', () => {
+    burgerMenu.classList.toggle('burger-menu_opened');
+
+});
+main.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger-menu_opened')
+});
+
+// Открытие формы спасибо
+
+const popupThanks = new Popup(thanksPopup, popupCloseReg);
+
+confirmButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    popupThanks.open();
+    popupReg.close();
+});
+
+// Открытие восстановления пароля
+
+const popupRec = new Popup(modalRec, popupCloseReg);
+
+popupLink.addEventListener('click', () => {
+    popupLk.close();
+    popupRec.open();
+
+});
+
+// Пароль восстановлен
+const popupReset = new Popup(modalReset, popupCloseReg);
+
+recButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    popupReset.open();
+    popupRec.close();
+});
+// Тарифы
+const popupPlug = new Popup(modalPlug, closePlug);
+
+toPlug.forEach((item) => {
+   item.addEventListener('click', () => {
+       popupPlug.open();
+   })
+});
+
+// Прибавление и уменьшение числа
 
 const minusUp = document.querySelector('.calculate-block__minus_up');
 const plusUp = document.querySelector('.calculate-block__minus_plus-up');
@@ -34,33 +120,5 @@ countBlockDown.addEventListener('click', (event) => {
     countUp(event, numberDown, plusDown, minusDown)
 });
 
-
-
-const openModal = () => {
-    modal.classList.add('modal_is-opened');
-};
-
-popupClose.addEventListener('click', () => {
-
-    modal.classList.remove('modal_is-opened');
-});
-
-lkLink.addEventListener('click', () => {
-    openModal();
-    burgerMenu.classList.remove('burger-menu_opened');
-
-});
-
-lkButton.addEventListener('click', openModal);
-
-
-burgerButton.addEventListener('click', () => {
-    burgerMenu.classList.toggle('burger-menu_opened');
-
-});
-main.addEventListener('click', () => {
-    burgerMenu.classList.remove('burger-menu_opened');
-
-});
 
 
