3921022ad0a90bf281c0d334206bf1917004cf44
vladoskav <vladoskavkniaz@yandex.ru>
2020-07-22 12:47:53 +0300

    fix: add another header to lk

--- src/js/lkCompany.js
+++ src/js/lkCompany.js
@@ -5,14 +5,9 @@ import Popup from "./popup";
 // Селекторы
 
 
-const lkButton = document.querySelector('.header__lk-button');
 const burgerButton = document.querySelector('.burger-menu__button');
 const burgerMenu = document.querySelector('.burger-menu');
 const main = document.querySelector('.main');
-const modalLk = document.querySelector('.modal_lk');
-const popupCloseLk = document.querySelector('.popup__close_lk');
-const startWorkButton = document.querySelector('.header__start-button');
-const modalReg = document.querySelector('.modal_reg');
 const popupCloseReg = document.querySelector('.popup__close_reg');
 const confirmButton = document.querySelector('.popup__button_reg');
 const thanksPopup = document.querySelector('.modal_thanks');
@@ -20,21 +15,6 @@ const popupLink = document.querySelector('.popup__link');
 const modalRec = document.querySelector('.modal_recovery');
 const recButton = document.querySelector('.popup__button_recovery');
 const modalReset = document.querySelector('.modal_reset');
-// Открытие личного кабинета
-const popupLk = new Popup(modalLk, popupCloseLk);
-
-
-lkButton.addEventListener('click', () => {
-    popupLk.open();
-});
-
-// Открытие регистрации
-const popupReg = new Popup(modalReg, popupCloseReg);
-
-startWorkButton.addEventListener('click', () => {
-    popupReg.open();
-
-});
 
 // Открытие мобильного меню
 
